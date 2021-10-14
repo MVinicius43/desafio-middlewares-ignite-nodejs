acc76300fe3fcc5fab85f0dcfebc7a6bd3ff9799
Marcos Silva (PSV) <marcos.silva@psvsistemas.com.br>
2021-10-14 13:09:47 -0300

    final commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.' })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.' })
+  }
+
+  if (user.pro) {
+    return next()
+  } else if (user.todos.length < 10) {
+    return next()
+  } else {
+    return response.status(403).json({ error: 'You already have 10 todos registers. Update your plan.' })
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.some(user => user.username === username)
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found.' })
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Id not valid.' })
+  }
+
+  if (todo === undefined) {
+    return response.status(404).json({ error: 'Id not found.' })
+  }
+
+  request.todo = todo
+  request.user = user
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  
+  const user = users.find(user => user.id === id)
+
+  if (user === undefined) {
+    return response.status(404).json({ error: 'User not found.' })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
